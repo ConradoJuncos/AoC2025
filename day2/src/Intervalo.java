b6eb1777503af28fbb00df2a7d508c23fc35817c
Conrado <conryjuncon@hotmail.com>
2025-12-10 09:57:08 -0300

    day2pt2

--- day2/src/Intervalo.java
+++ day2/src/Intervalo.java
@@ -1,26 +1,66 @@
 public class Intervalo {
     long a;
     long b;
-    private long sum;
+    private long sum1;
+    private long sum2;
 
     public Intervalo(long a, long b) {
         this.a = a;
         this.b = b;
-        this.sum = 0;
+        this.sum1 = 0;
+        this.sum2 = 0;
     }
 
-    public long getSumIdInvalidos() {
+    public long getSumIdInvalidos1() {
         for (long i = a; i <= b; i++) {
             String strI = String.valueOf(i);
             if (strI.length() % 2 != 0) {
                 continue;
             }
             if (strI.substring(0, (strI.length() / 2)).equals(strI.substring(strI.length() / 2))) {
-                sum += Long.parseLong(strI);
-//                IO.println(strI);
+                sum1 += Long.parseLong(strI);
             }
         }
-        return sum;
+        return sum1;
     }
 
+    public long getSumIdInvalidos2() {
+        for (long i = a; i <= b; i++) {
+            String strI = String.valueOf(i);
+            if (idIsInvalid2(strI)) {
+                sum2 += Long.parseLong(strI);
+            }
+        }
+        return sum2;
+    }
+
+    public boolean idIsInvalid2(String strI) {
+        boolean flag;
+        // for que determina cuales son los strings a comparar
+        for (int i = 1; i <= strI.length() / 2; i++) {
+            // Substring base a comparar
+            String comparingSubstring = strI.substring(0, i);
+
+            flag = true;
+            // for que compara el string con cada otro substring
+            for (int j = 1; j < strI.length() / comparingSubstring.length(); j++) {
+                // Flag = false si la longitud del string a comparar no es un multiplo de la longitud del string original
+                if (strI.length() % comparingSubstring.length() != 0) {
+                    flag = false;
+                }
+                // Substrings que comparo con el base
+                String longString = strI.substring(comparingSubstring.length() * j, comparingSubstring.length() * (j + 1));
+                // Si son distintos seteo flag a false y corto el ciclo
+                if (!longString.equals(comparingSubstring)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return true;
+            }
+        }
+        // Si ningun string a comparar devolvio true, entonces no cumple la condicion y retorna false
+        return false;
+    }
 }
--- day2/src/Main.java
+++ day2/src/Main.java
@@ -1,16 +1,21 @@
-void main() {
+void main(String[] args) {
     try (FileReader f = new FileReader("day2/src/resources/input")) {
         String instructionsString = f.readAllAsString();
         String[] instructions = instructionsString.split(",");
 
-        long sum = 0;
+        long sum1 = 0;
+        long sum2 = 0;
         for (String i : instructions) {
             String[] ranges = i.split("-");
             List<String> rangesList = Arrays.stream(ranges).toList();
             Intervalo intervalo = new Intervalo(Long.parseLong(rangesList.get(0)), Long.parseLong(rangesList.get(1)));
-            sum += intervalo.getSumIdInvalidos();
+            sum1 += intervalo.getSumIdInvalidos1();
+            sum2 += intervalo.getSumIdInvalidos2();
         }
-        IO.println("Suma: " + sum);
+        IO.println();
+        IO.println("Suma 1: " + sum1);
+        IO.println("Suma 2: " + sum2);
+        IO.println();
 
     } catch (Exception e) {
         IO.println("Excepcion: " + e.getMessage());
